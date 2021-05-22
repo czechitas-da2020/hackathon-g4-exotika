3b1b1e11440decfae86b1cfdbf72405b9a025152
Hanka Jakubickova <hanka.jakubicek@gmail.com>
2021-05-22 18:55:09 +0200

    Login in header test created

---
+++ .history/test/specs/login.header.spec_20210522185240.js
@@ -0,0 +1,12 @@
+describe('Client open the application', () => {
+    before (() => {
+        browser.url('http://czechitas-datestovani-hackathon.cz/en/')
+    })
+
+    it('Through Sign in button client is redirected to Authentication page', ()=>{
+            browser.$('.hide_xs').click()
+            let create = browser.$()
+            let signIn = browser.$('#SubmitLogin')
+            expect(signIn).toBeExisting()                             
+    })
+})
---
+++ .history/test/specs/login.header.spec_20210522185321.js
@@ -0,0 +1,13 @@
+describe('Client open the application', () => {
+    before (() => {
+        browser.url('http://czechitas-datestovani-hackathon.cz/en/')
+    })
+
+    it('Through Sign in button client is redirected to Authentication page', ()=>{
+            browser.$('.hide_xs').click()
+            let create = browser.$('#SubmitCreate')
+            expect(create).toBe
+            let signIn = browser.$('#SubmitLogin')
+            expect(signIn).toBeExisting()                             
+    })
+})
